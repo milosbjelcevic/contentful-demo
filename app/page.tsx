91fbdcdd1608c43c6bd955a937ec4fb8fd542920
Milos Bjelcevic <bjem@ip-192-168-0-25.eu-west-1.compute.internal>
2024-07-22 15:39:36 +0200

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -62,6 +62,7 @@ export default async function Page() {
   const { isEnabled } = draftMode();
   const allPosts = await getAllPosts(isEnabled);
   const heroPost = allPosts[0];
+
   const morePosts = allPosts.slice(1);
 
   return (
